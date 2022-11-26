b755c3c758f09385041e6ac3b444d6535ed8da8f
Michkasov Roman <mihjob@list.ru>
2022-11-27 01:05:40 +0300

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int GetAckermannValue(int number1, int number2)
+{
+    if (number1 == 0) return number1 + 1;
+    if else (number1 > 0 && number2 == 0)  return GetAckermannValue(number1 - 1, 1);
+    else 
+}
