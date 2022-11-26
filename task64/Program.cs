965680988c87c004a960af72b76ddb6c8d7efaf9
Michkasov Roman <mihjob@list.ru>
2022-11-26 21:20:47 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string GetReverseOrder(int number)
+{
+    if (number == 1) return number.ToString();
+    else
+    return $"{GetReverseOrder(number - 1)}"; 
+}
+
+Console.Clear();
+int m = int.Parse(Console.ReadLine()!);
+string a = GetReverseOrder(m);
+Console.Write(a);
